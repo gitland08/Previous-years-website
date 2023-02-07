d2112bd4e338da374b3cc4007950f0d079c7eedc
prathamkandari <prathamkandari123@gmail.com>
2023-02-07 23:22:26 +0530

    New Updates

--- scraping/upes_lib.py
+++ scraping/upes_lib.py
@@ -8,12 +8,8 @@ url = "http://library.ddn.upes.ac.in:8081/upeslib/questionbank/soc/btech_AI.html
 # For getting the HTMl
 r = requests.get(url)
 htmlContent = r.content
-# print(htmlContent)
 # HTML parser
 soup = BeautifulSoup(htmlContent, 'html.parser')
-# print(soup.prettify)
-
-# lists = soup.findAll('a',class_="LINK")
 
 list2=[]
 list3=[]
@@ -27,23 +23,5 @@ del list2[0]
 print(list2)
 
 
-
-# fields= ['LINKS']
-# with open('CSV File.csv', 'w+') as f:
-    # csv_writer = csv.writer(f)
-    # csv_writer.writerow(fields)
-    # csv_writer.writerow(list2)
-# for line in lists:
-    # for i in line:
-        # print(i)
-    # print(line)
-
-# for i in range(len(lists)):
-#     print(list2[i])
-
-
-# df = pd.DataFrame(list2, columns=['Links'])
-# print(df)
-# df.to_csv('CSV File.csv')
 
 
