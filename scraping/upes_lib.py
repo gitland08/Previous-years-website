4a0136e80143a5e94f4973b2d8eddeacddd3cf79
prathamkandari <prathamkandari123@gmail.com>
2023-02-08 22:15:28 +0530

    New Update

--- scraping/upes_lib.py
+++ scraping/upes_lib.py
@@ -12,16 +12,21 @@ htmlContent = r.content
 soup = BeautifulSoup(htmlContent, 'html.parser')
 
 list2=[]
-list3=[]
-list4=[]
 
 for link in soup.find_all('a'):
     list2.append(link.get('href'))
 
 del list2[0:9]
 del list2[0]
-print(list2)
+
+fields= ['LINKS']
+with open('CSV File.csv', 'w+') as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(fields)
+    csv_writer.writerow(list2)
 
 
+df = pd.DataFrame(list2, columns=['Links'])
+df.to_csv('CSV File.csv')
 
 
